19ca6e8c4835213d4f09449199c8555de407843b
doye <edyogoba23@gmail.com>
2025-04-27 20:07:40 +0100

    fix: Update middleware path matcher to secure specific game routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,5 +4,5 @@ export default authMiddleware()
 
 export const config = {
   // include the paths you wish to secure here
-  matcher: [ '/((?!_next|favicon.ico|sitemap.xml|robots.txt).*)'], 
+  matcher: [ '/game-play', '/play', '/create-game'], 
 }
